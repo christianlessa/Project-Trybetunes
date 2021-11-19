268a426b5bfb5acf9478052bf99121115b1a75e1
Christian Lessa <christianclessa@gmail.com>
2021-11-18 23:26:35 -0300

    Requisito 1 concluido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route exact patch="/" component={ Login } />
+        <Route patch="/" />
+        <Route patch="/search" component={ Search } />
+        <Route patch="/album/:id" component={ Album } />
+        <Route patch="/favorites" component={ Favorites } />
+        <Route patch="/profile" component={ Profile } />
+        <Route patch="/profile/edit" component={ ProfileEdit } />
+        <Route patch="*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
