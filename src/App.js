880f540f25da8457d2097041c7adb310399fe872
Christian Lessa <christianclessa@gmail.com>
2021-11-22 15:15:46 -0300

    Requisito 2 concluido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,19 +7,22 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import Loading from './pages/Loading';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact patch="/" component={ Login } />
-        <Route patch="/" />
-        <Route patch="/search" component={ Search } />
-        <Route patch="/album/:id" component={ Album } />
-        <Route patch="/favorites" component={ Favorites } />
-        <Route patch="/profile" component={ Profile } />
-        <Route patch="/profile/edit" component={ ProfileEdit } />
-        <Route patch="*" component={ NotFound } />
+        <Switch>
+          <Route exact patch="/" component={ Login } />
+          <Route patch="/search" component={ Search } />
+          <Route patch="/album/:id" component={ Album } />
+          <Route patch="/favorites" component={ Favorites } />
+          <Route patch="/profile" component={ Profile } />
+          <Route patch="/profile/edit" component={ ProfileEdit } />
+          <Route patch="*" component={ NotFound } />
+          <Route patch="/loading" component={ Loading } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      buttonStatus: true,
+      isLoading: false,
+      isLoaded: false,
+    };
+    this.inputValue = this.inputValue.bind(this);
+    this.buttonDisable = this.buttonDisable.bind(this);
+    this.saveName = this.saveName.bind(this);
+  }
+
+  inputValue(event) {
+    this.setState({ name: event.target.value },
+      () => this.setState({ buttonStatus: this.buttonDisable() }));
+  }
+
+  buttonDisable() {
+    const three = 3;
+    const { name } = this.state;
+    if (name.length >= three) return false;
+    return true;
+  }
+
+  async saveName() {
+    this.setState({ isLoading: true });
+    const { name } = this.state;
+
+    await createUser({ name });
+    this.setState({ isLoaded: true });
+  }
+
   render() {
+    const { buttonStatus, isLoading, isLoaded } = this.state;
+    if (isLoaded) return <Redirect to="/search" />;
+
     return (
-      <div data-testid="page-login">
-        <h1>Login:</h1>
-      </div>
+      isLoading ? <Loading /> : (
+        <form>
+          <div data-testid="page-login">
+            <h1>Login:</h1>
+            <label htmlFor="name">
+              <p>Name:</p>
+              <input
+                type="text"
+                onChange={ this.inputValue }
+                name="name"
+                data-testid="login-name-input"
+              />
+              <button
+                onClick={ this.saveName }
+                disabled={ buttonStatus }
+                type="button"
+                data-testid="login-submit-button"
+              >
+                Entrar
+              </button>
+            </label>
+          </div>
+        </form>
+      )
     );
   }
 }
