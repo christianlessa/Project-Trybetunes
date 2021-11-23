7c08b36d3d18ed9179d87172f4cf858036b20532
Christian Lessa <christianclessa@gmail.com>
2021-11-22 22:04:13 -0300

    Requisito 5 concluido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonStatus: true,
+
+    };
+    this.inputValue = this.inputValue.bind(this);
+  }
+
+  inputValue({ target: { value } }) {
+    const two = 2;
+
+    this.setState({
+      buttonStatus: value.length < two,
+    });
+  }
+
   render() {
+    const { buttonStatus } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search" />
+        <label htmlFor="artist">
+          <input
+            type="text"
+            id="artist"
+            onChange={ this.inputValue }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            disabled={ buttonStatus }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </label>
       </>
     );
   }
