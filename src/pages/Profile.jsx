d5b874aafb74e45b97a1b13621f0d533b0a2a948
Christian Lessa <christianclessa@gmail.com>
2021-12-22 18:19:00 -0300

    Requisito 13 concluido

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
+import Loading from './Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+    };
+    this.profileUser = this.profileUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.profileUser();
+  }
+
+  async profileUser() {
+    this.setState({ isLoading: true });
+    const response = await getUser();
+    this.setState({
+      isLoading: false,
+      name: response.name,
+      email: response.email,
+      image: response.image,
+      description: response.description,
+    });
+  }
+
   render() {
+    const {
+      isLoading,
+      name,
+      email,
+      image,
+      description } = this.state;
+
     return (
       <>
         <Header />
-        <div data-testid="page-profile">
-          <h1>Profile:</h1>
-        </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div data-testid="page-profile">
+            <img data-testid="profile-image" src={ image } alt="foto do perfil" />
+            <p>{name}</p>
+            <p>{email}</p>
+            <p>{description}</p>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
       </>
     );
   }
